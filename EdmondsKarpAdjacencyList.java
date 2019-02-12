2a38727e24941d52ce096f0856c1b9f7dda08f70
AlexShimanovich <37244900+AlexShimanovich@users.noreply.github.com>
2019-02-12 14:20:34 +0200

    Stable min cut max flow, cut in edges or vertices!

---
+++ EdmondsKarpAdjacencyList.java
@@ -0,0 +1,402 @@
+package testing;
+/**
+ * An implementation of the Edmonds-Karp algorithm which is essentially
+ * Ford-Fulkerson with a BFS as a method of finding augmenting paths. 
+ * This Edmonds-Karp algorithm will allow you to find the max flow through
+ * a directed graph and the min cut as a byproduct. 
+ *
+ * Time Complexity: O(VE^2)
+ * 
+ * @author William Fiset, william.alexandre.fiset@gmail.com
+ **/
+//package com.williamfiset.algorithms.graphtheory.networkflow;
+
+import static java.lang.Math.min;
+
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.StreamTokenizer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import testing.NetworkFlowSolverBase.Edge;
+
+public class EdmondsKarpAdjacencyList extends NetworkFlowSolverBase {
+
+	/**
+	 * Creates an instance of a flow network solver. Use the {@link #addEdge(int, int, int)}
+	 * method to add edges to the graph.
+	 *
+	 * @param n - The number of nodes in the graph including source and sink nodes.
+	 * @param s - The index of the source node, 0 <= s < n
+	 * @param t - The index of the sink node, 0 <= t < n, t != s
+	 */
+	public EdmondsKarpAdjacencyList(int n, int s, int t) {
+		super(n, s, t);
+	}
+
+	// Run Edmonds-Karp and compute the max flow from the source to the sink node.
+	@Override
+	public void solveBFS() {
+		long flow;
+		do {
+			markAllNodesAsUnvisited();
+			flow = bfs(maxFlow);
+			maxFlow += flow;
+		} while (flow != 0);
+
+		for(int i = 0; i < n; i++)
+			if (visited(i))
+				leftOfCut[i] = true;
+	}
+	
+	@Override
+	public void bigSolveBFS() {
+		long flow;
+		do {
+			bigMarkAllNodesAsUnvisited();
+			flow = bigBfs(bigMaxFlow);
+			bigMaxFlow += flow;
+		} while (flow != 0);
+
+		for(int i = 0; i < bigN; i++) {
+			if (bigVisited(i)) {
+				bigLeftOfCut[i] = true;
+				for(Edge edge : cutEdges) {
+					if(edge.to == i)
+						cutEdges.remove(edge);
+
+				}
+			}
+		}
+		
+		for(Edge edge : cutEdges) {
+			originalCutNodes.add(edge.from / 2);
+		}
+		
+		
+	}
+	
+	private long floww() {
+		long flow = 0;
+		for (Edge e : cutEdges)
+			flow += e.capacity;
+		return flow;	
+	}
+
+	private long bfs(long CurrentFlow) {
+		Edge[] prev = new Edge[n];
+		cutEdges.clear();
+		originalCutNodes.clear();
+		// The queue can be optimized to use a faster queue
+		Queue<Integer> q = new ArrayDeque<>(n);
+		visit(s);
+		q.offer(s);
+
+		// Perform BFS from source to sink
+		while(!q.isEmpty()) {
+			int node = q.poll();
+			if (node == t) break;
+
+			for (Edge edge : graph[node]) {
+				long cap = edge.remainingCapacity();
+				if (cap > 0 && !visited(edge.to)) {
+					visit(edge.to);
+					prev[edge.to] = edge;
+					q.offer(edge.to);
+				}
+				if (cap == 0 && !edge.isResidual() && floww() < CurrentFlow) {
+					cutEdges.add(edge);
+					if(!originalCutNodes.contains(edge.from))
+						originalCutNodes.add(edge.from);					
+				}								
+			}
+		}
+
+		// Sink not reachable!
+		if (prev[t] == null) return 0;
+
+		long bottleNeck = Long.MAX_VALUE;
+
+		// Find augmented path and bottle neck
+		for(Edge edge = prev[t]; edge != null; edge = prev[edge.from])
+			bottleNeck = min(bottleNeck, edge.remainingCapacity());
+
+		// Retrace augmented path and update flow values.
+		for(Edge edge = prev[t]; edge != null; edge = prev[edge.from])
+			edge.augment(bottleNeck);
+
+		// Return bottleneck flow
+		return bottleNeck;
+	}
+
+	private long bigBfs(long CurrentFlow) {
+		Edge[] prev = new Edge[bigN];
+		cutEdges.clear();
+		// The queue can be optimized to use a faster queue
+		Queue<Integer> q = new ArrayDeque<>(bigN);
+		bigVisit(bigS);
+		q.offer(bigS);
+		
+		System.out.println("START bfsssssssssss, current flow: " + CurrentFlow );
+		// Perform BFS from source to sink
+		while(!q.isEmpty()) {
+			int node = q.poll();
+			if (node == bigT) break;
+			System.out.println("node " + node );
+			for (Edge edge : bigGraph[node]) {
+				long cap = edge.remainingCapacity();
+				if (cap > 0 && !bigVisited(edge.to)) {
+					bigVisit(edge.to);
+					prev[edge.to] = edge;
+					q.offer(edge.to);
+				}
+				System.out.println("edge " + edge.toString(edge.from, edge.to) );
+				if (cap == 0 && !edge.isResidual() && floww() <= CurrentFlow) {
+					Edge x = prev[edge.to];
+					while(x != null) {
+						if(cutEdges.contains(x))
+							cutEdges.remove(x);
+						x = prev[x.to];
+					}
+					cutEdges.add(edge);				
+				}								
+			}
+		}
+		
+		// Sink not reachable!
+		if (prev[bigT] == null) return 0;
+
+		long bottleNeck = Long.MAX_VALUE;
+
+		// Find augmented path and bottle neck
+		for(Edge edge = prev[bigT]; edge != null; edge = prev[edge.from])
+			bottleNeck = min(bottleNeck, edge.remainingCapacity());
+
+		// Retrace augmented path and update flow values.
+		for(Edge edge = prev[bigT]; edge != null; edge = prev[edge.from])
+			edge.augment(bottleNeck);
+
+		// Return bottleneck flow
+		return bottleNeck;
+	}
+
+
+	/* Example */
+	//"C:\\Users\\JERLocal\\eclipse-workspace\\testing\\src\\testing\\alex.txt"
+	public static void main(String[] args) throws IOException, InterruptedException {
+		long startTimeBFS = 0;//System.nanoTime();
+		long endTimeBFS = 0;//System.nanoTime();
+		long startTimeDFS = 0;//System.nanoTime();
+		long endTimeDFS = 0;//System.nanoTime();
+		testSmallFlowGraph();
+
+	}
+
+	// Testing graph from:
+	// http://crypto.cs.mcgill.ca/~crepeau/COMP251/KeyNoteSlides/07demo-maxflowCS-C.pdf
+	private static void testSmallFlowGraph() {
+
+		//List<Edge>[] originalGraph = ;
+		int n = 9;
+		int s = 0;
+		int t = 8;
+
+		EdmondsKarpAdjacencyList solver;
+		solver = new EdmondsKarpAdjacencyList(n, s, t);
+
+		// Source edges
+		solver.addEdge(0, 1, 1);
+		solver.addEdge(0, 2, 1);
+		solver.addEdge(0, 3, 1);
+//		solver.addEdge(1,5,9);
+		solver.addEdge(1, 4, 1);
+		solver.addEdge(2, 4, 1);
+		solver.addEdge(3, 4, 1);
+		
+		solver.addEdge(4, 5, 1);
+		solver.addEdge(4, 6, 1);
+		solver.addEdge(4, 7, 1);
+		
+		solver.addEdge(5, 8, 1);
+		solver.addEdge(6, 8, 1);
+		solver.addEdge(7, 8, 1);
+		/*
+		int n = 5;
+		int s = 1;
+		int t = 5;
+
+		EdmondsKarpAdjacencyList solver;
+		solver = new EdmondsKarpAdjacencyList(n, s, t);
+
+		// Source edges
+		solver.addEdge(1, 2, 1);
+		solver.addEdge(1, 3, 1);
+		solver.addEdge(1, 4, 1);
+		
+		solver.deepGraphBackup(solver.bigGraph);*/
+
+		System.out.println("Max flow is: " + solver.getMaxFlow(true));
+		System.out.println("Nodes Left of cut:");
+		boolean[] x = solver.getMinCut(true); 
+		for(int i = 0; i < x.length; i++){
+			if(x[i])
+				System.out.println(i);
+		}
+
+		
+//		System.out.println("");
+//		List<Edge>[] graph = solver.getGraph(false);
+//		for(int i = 0; i < graph.length; i++){
+//			for(Edge e : graph[i]){
+//				if(e.isMinCut() && !e.isResidual())
+//					System.out.println(e.toString(s, t));
+//			}
+//		}
+		
+		System.out.println("Cut Nodes in original graph:");
+		for(int node : originalCutNodes) {
+			System.out.println(node);			
+		}
+		System.out.println("Cut Edges in big graph:");
+		for(Edge e : cutEdges) {
+			System.out.println(e.toString(s, t));			
+		}
+	}
+
+	/*
+	 * Build graph from file, first line is num of vertices, source vertex num, target vertex num
+	 * all other lines are edges: from vertex num, to vertex num, capacity of the edge
+	 */
+	private static EdmondsKarpAdjacencyList buildGraphfromFile(String filePath) throws IOException {
+		EdmondsKarpAdjacencyList solver;
+		LineNumberReader lnr = new LineNumberReader(new FileReader(filePath));
+		lnr.setLineNumber(1);
+		StreamTokenizer stok = new StreamTokenizer(lnr);
+		stok.parseNumbers();
+		stok.eolIsSignificant(true);
+		stok.nextToken();
+		int vertices = 0, source = 0, target = 0;
+		//parse first line
+		while (stok.ttype != StreamTokenizer.TT_EOL) {
+			if (stok.ttype == StreamTokenizer.TT_NUMBER)
+				vertices = (int)stok.nval;
+			stok.nextToken();
+			if (stok.ttype == StreamTokenizer.TT_NUMBER)
+				source = (int)stok.nval;
+			stok.nextToken();
+			if (stok.ttype == StreamTokenizer.TT_NUMBER)
+				target = (int)stok.nval;
+			stok.nextToken();
+		}
+		//System.out.println("new graph, edges: " + vertices + " source: " + source + " target: " + target);
+		solver = new EdmondsKarpAdjacencyList(vertices, source, target);
+		stok.nextToken();
+		int from = 0, to = 0, capacity = 0;
+		//parse all other lines, each line is edge
+		while (stok.ttype != StreamTokenizer.TT_EOF) {
+			int lineNum = lnr.getLineNumber();
+			double sum = 0;
+			while (stok.ttype != StreamTokenizer.TT_EOL) {
+				if (stok.ttype == StreamTokenizer.TT_NUMBER)
+					from = (int)stok.nval;
+				stok.nextToken();
+				if (stok.ttype == StreamTokenizer.TT_NUMBER)
+					to = (int)stok.nval;
+				stok.nextToken();
+				if (stok.ttype == StreamTokenizer.TT_NUMBER)
+					capacity = (int)stok.nval;
+				stok.nextToken();
+			}
+			//System.out.println("edge in line: " + lineNum + " from: " + from + " to: " + to + " capacity: " + capacity);
+			if(from == source || to == target)
+				capacity = Integer.MAX_VALUE / 2;
+			solver.addEdge(from, to, capacity);
+			stok.nextToken();
+		}
+		lnr.close();
+		return solver;
+
+	}
+
+	/*
+	 * Create random graph of given size and represent it in a file
+	 */
+	private static Boolean fillFile(String filePath, int vertices) throws IOException, InterruptedException  {
+		Path fileToDeletePath = Paths.get(filePath);
+		Files.deleteIfExists(fileToDeletePath);
+		BufferedWriter writer = null;
+		try {
+			writer = new BufferedWriter(new FileWriter(filePath, true));
+		}	
+		catch( java.io.FileNotFoundException ex) {
+			System.out.println("Access denied will retry in 3 sec");
+			Thread.sleep(3000);
+			writer = new BufferedWriter(new FileWriter(filePath, true));
+		}
+
+
+		String line = "";
+		line = line.concat("" + vertices + " "); //how many vertices
+		line = line.concat("" + 0 + " "); //source
+		line = line.concat("" + (vertices - 1) + "\n"); //target
+		writer.write(line);
+
+
+		Random rand = new Random();
+		List < Integer > usedVertices = new ArrayList <  > ();
+		int numOfEdges = 0;
+		int toVertex = 0;
+		int rightMostVertex = 0;
+		int leftMostVertex = 0;
+		int capacity = 0;
+		int possibleEdges = 20;//vertices / ; //how many edges max go out from one vertex 
+		for(int fromVertex = 0; fromVertex < vertices - 1; fromVertex++) { //go over all edges except target
+			//decide how many edges will go from this vertex
+			numOfEdges = possibleEdges;//rand.nextInt(possibleEdges) + 1; //no more than quarter of max possible edges and at least 1 edge 
+			usedVertices.add(fromVertex); //add current fromVertex to list of used vertices to avoid self pointing
+			//limits for edges going from this vertex, go more right than left?
+			rightMostVertex = Math.min((fromVertex + (int)(possibleEdges)), (vertices - 1));
+			leftMostVertex = Math.max(fromVertex - (int)(possibleEdges), 0);
+			//add to file all edges going from current fromVertex
+			for(int edge = 0; edge < numOfEdges; edge++) { 
+				do {//choose the destination of edge, can be any vertex between leftmost and rightmost
+					toVertex = rand.nextInt(rightMostVertex - leftMostVertex + 1) + leftMostVertex; 
+				}while(usedVertices.contains(toVertex));
+				usedVertices.add(toVertex); //add the toVertex to the list of used vertices, to avoid multiple edges between same nodes
+				line = "";
+				line = line.concat("" + fromVertex + " ");
+				line = line.concat("" + toVertex + " ");
+				capacity =  1;
+				line = line.concat("" + capacity + "\n"); //capacity is always 1?
+				writer.write(line);
+
+				//System.out.println(line);			
+
+			}
+			//last quarter of vertex always connect to target
+			//			if((fromVertex >= vertices - possibleEdges) && !usedVertices.contains(vertices - 1)) {
+			//				System.out.println("last quarter");
+			//				line = "";
+			//				line = line.concat("" + fromVertex + " ");
+			//				line = line.concat("" + (vertices - 1) + " ");
+			//				capacity = rand.nextInt(10) + 1;
+			//				line = line.concat("" + capacity + "\n"); //capacity is always 1?
+			//				System.out.println("last quarter line " + line);
+			//				writer.write(line);
+			//			}
+			usedVertices.clear();//clean the list of used vertices before moving to next fromVertex
+		}
+		line = "\n";
+		writer.write(line);
+		writer.close();
+		return true;
+		//System.out.println("done filling file");
+	}
+}
---
+++ NetworkFlowSolverBase.java
@@ -0,0 +1,292 @@
+package testing;
+
+/**
+ * @author William Fiset, william.alexandre.fiset@gmail.com
+ **/
+//package com.williamfiset.algorithms.graphtheory.networkflow;
+
+import static java.lang.Math.min;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class NetworkFlowSolverBase {
+
+	// To avoid overflow, set infinity to a value less than Long.MAX_VALUE;
+	protected static final long INF = Long.MAX_VALUE / 2;
+
+	public static class Edge {
+		public int from, to;
+		public Edge residual;
+		public long flow, cost;
+		public final long capacity, originalCost;
+
+		public Edge(int from, int to, long capacity) {
+			this(from, to, capacity, 0 /* unused */);
+		}
+
+		public Edge(int from, int to, long capacity, long cost) {
+			this.from = from;
+			this.to = to;
+			this.capacity = capacity;
+			this.originalCost = this.cost = cost;
+		}
+
+		public boolean isResidual() {
+			return capacity == 0;
+		}
+
+		public long remainingCapacity() {
+			return capacity - flow;
+		}
+
+		public void augment(long bottleNeck) {
+			flow += bottleNeck;
+			residual.flow -= bottleNeck;
+		}
+
+		public String toString(int s, int t) {
+//			String u = (from == s) ? "s" : ((from == t) ? "t" : String.valueOf(from));
+//			String v = (to == s) ? "s" : ((to == t) ? "t" : String.valueOf(to));
+			String u = String.valueOf(from);
+			String v =  String.valueOf(to);
+			return String.format("Edge %s -> %s | flow = %d | capacity = %d | is residual: %s", 
+					u, v, flow, capacity, isResidual());
+			
+		}
+		
+		public boolean isMinCut() {
+			return capacity == flow;
+		}
+	}
+
+	// Inputs: n = number of nodes, s = source, t = sink
+	protected final int n, s, t;
+	protected final int bigN, bigS, bigT;
+
+	protected long maxFlow;
+	protected long bigMaxFlow;
+	protected long minCost; //not relevant
+
+	protected boolean[] leftOfCut;
+	protected boolean[] bigLeftOfCut;
+	protected static ArrayList<Integer> originalCutNodes = new ArrayList<Integer>();
+	protected List<Edge>[] graph;
+	protected List<Edge>[] bigGraph;
+
+	protected static ArrayList<Edge> cutEdges = new ArrayList<Edge>();
+
+	// 'visited' and 'visitedToken' are variables used for graph sub-routines to 
+	// track whether a node has been visited or not. In particular, node 'i' was 
+	// recently visited if visited[i] == visitedToken is true. This is handy 
+	// because to mark all nodes as unvisited simply increment the visitedToken.
+	private int visitedToken = 1;
+	private int bigVisitedToken = 1;
+	private int[] visited;
+	private int[] bigVisited;
+
+
+	// Indicates whether the network flow algorithm has ran. We should not need to
+	// run the solver multiple times, because it always yields the same result.
+	private boolean solvedBFS;	
+	private boolean bigSolvedBFS;
+
+
+	/**
+	 * Creates an instance of a flow network solver. Use the {@link #addEdge}
+	 * method to add edges to the graph.
+	 *
+	 * @param n - The number of nodes in the graph including source and sink nodes.
+	 * @param s - The index of the source node, 0 <= s < n
+	 * @param t - The index of the sink node, 0 <= t < n, t != s
+	 */
+	public NetworkFlowSolverBase(int n, int s, int t) {
+		this.n = n; this.s = s; this.t = t; 
+		this.bigN = n * 2; this.bigS = s * 2; this.bigT = t * 2; 
+		initializeGraph();
+		leftOfCut = new boolean[n];
+		visited = new int[n];
+		bigLeftOfCut = new boolean[bigN];
+		bigVisited = new int[bigN];
+	}
+
+	// Construct an empty graph with n nodes including the source and sink nodes.
+	private void initializeGraph() {
+		graph = new List[n];
+		for (int i = s; i <= t; i++)
+			graph[i] = new ArrayList<Edge>();
+	}
+
+	/**
+	 * Adds a directed edge (and residual edge) to the flow graph.
+	 *
+	 * @param from     - The index of the node the directed edge starts at.
+	 * @param to       - The index of the node the directed edge ends at.
+	 * @param capacity - The capacity of the edge.
+	 */
+	public void addEdge(int from, int to, long capacity) {
+		if (capacity < 0) throw new IllegalArgumentException("Capacity < 0");
+		Edge e1 = new Edge(from, to, capacity);
+		Edge e2 = new Edge(to, from, 0);
+		e1.residual = e2;
+		e2.residual = e1;
+		graph[from].add(e1);
+		graph[to].add(e2);
+	}
+	
+	/*
+	public void deepGraphBackup(List<Edge>[] bigGraph) {
+		for (int i = 1; i < bigGraph.length; i++) 
+			for (Edge e : bigGraph[i]) {
+				Edge temp = new Edge(e.from, e.to, e.capacity);
+				bigGraphBackup[i].add(temp);	
+			}
+				 					
+	}
+	
+	public void deepGraphRestore() {
+		for (int i = 1; i < bigGraphBackup.length; i++) 
+			for (Edge e : bigGraphBackup[i]) {
+				Edge temp = new Edge(e.from, e.to, e.capacity);
+				bigGraph[i].add(temp);	
+			}				 					
+	}*/
+	
+	//create big graph based on conversion of real graph - each vertex becomes two vertices and edge
+	//create edge from each vertices, make it minimal capacity (1) so it will be selected as min cut
+	//so we can know which nodes in original graph are the cut nodes
+	public void initBigGraph() {
+		bigGraph = new List[bigN];
+		for (int i = 0; i < bigGraph.length; i++) {
+			bigGraph[i] = new ArrayList<Edge>();
+		}
+		
+		for (int i = s; i < t; i++) {
+			int newCapacity = 1;
+			if(i == s || i == t)
+				newCapacity = bigGraph.length * 2; //if its sink or source node we make its edge capacity large
+			int newFrom = (i * 2);
+			int newTo = (i * 2) + 1;
+			Edge e1 = new Edge(newFrom, newTo, newCapacity); 
+			Edge e2 = new Edge(newTo, newFrom, 0);
+			e1.residual = e2;
+			e2.residual = e1;
+			bigGraph[newFrom].add(e1);
+			bigGraph[newTo].add(e2);
+			//convert existing edges
+			for(Edge edge : graph[i]) {	
+				if(!edge.isResidual()) {
+					Edge updatedEdge = new Edge((edge.from * 2) + 1, edge.to * 2, bigGraph.length * 2); //make original edges capacity very big so NOT selected as min cut of bigGraph
+					Edge updatedEdgeResidual = new Edge(edge.to * 2, (edge.from * 2) + 1, 0);
+					updatedEdge.residual = updatedEdgeResidual;
+					updatedEdgeResidual.residual = updatedEdge;
+					bigGraph[(edge.from * 2) + 1].add(updatedEdge);
+					bigGraph[edge.to * 2].add(updatedEdgeResidual);
+				}
+			}			
+		}
+		
+		
+		//print big graph
+		for (int i = 1; i < bigGraph.length; i++) {
+			System.out.println("big graph node i = " + i);
+			for (Edge e : bigGraph[i]) {
+				System.out.println(e.toString(e.from, e.to));
+				
+			}					
+		}
+				
+	}
+	
+	/** Cost variant of {@link #addEdge(int, int, int)} for min-cost max-flow */
+	public void addEdge(int from, int to, long capacity, long cost) {
+		Edge e1 = new Edge(from, to, capacity, cost);
+		Edge e2 = new Edge(to, from, 0, -cost);
+		e1.residual = e2;
+		e2.residual = e1;
+		graph[from].add(e1);
+		graph[to].add(e2);
+	}
+
+	// Marks node 'i' as visited.
+	public void visit(int i) {
+		visited[i] = visitedToken;
+	}
+	
+	public void bigVisit(int i) {
+		bigVisited[i] = bigVisitedToken;
+	}
+
+	// Returns whether or not node 'i' has been visited.
+	public boolean visited(int i) {
+		return visited[i] == visitedToken;
+	}
+	
+	public boolean bigVisited(int i) {
+		return bigVisited[i] == bigVisitedToken;
+	}
+
+	// Resets all nodes as unvisited. This is especially useful to do
+	// between iterations of finding augmenting paths, O(1)
+	public void markAllNodesAsUnvisited() {
+		visitedToken++;
+	}
+	
+	public void bigMarkAllNodesAsUnvisited() {
+		bigVisitedToken++;
+	}
+
+	/**
+	 * Returns the graph after the solver has been executed. This allow you to
+	 * inspect the {@link Edge#flow} compared to the {@link Edge#capacity} in 
+	 * each edge. This is useful if you want to figure out which edges were 
+	 * used during the max flow.
+	 */
+	public List<Edge>[] getGraph(boolean big) {
+		execute(big);
+		return big ? bigGraph : graph;
+	}
+
+	// Returns the maximum flow from the source to the sink.
+	public long getMaxFlow(boolean big) {
+		execute(big);
+		return big ? bigMaxFlow : maxFlow;
+	}
+
+	// Returns the min cost from the source to the sink.
+	// NOTE: This method only applies to min-cost max-flow algorithms.
+	public long getMinCost(boolean big) {
+		execute(big);
+		return minCost;
+	}
+
+	// Returns the min-cut of this flow network in which the nodes on the "left side"
+	// of the cut with the source are marked as true and those on the "right side" 
+	// of the cut with the sink are marked as false.
+	public boolean[] getMinCut(boolean big) {
+		execute(big);
+		return big ? bigLeftOfCut : leftOfCut;
+	}
+
+	// Wrapper method that ensures we only call solve() once
+	private void execute(boolean big) {
+		if (solvedBFS && !big || bigSolvedBFS && big) return;
+		if (!big) {
+			solvedBFS = true;
+			solveBFS();	
+			return;
+		}
+		//big
+		initBigGraph();
+		bigSolvedBFS = true;
+		bigSolveBFS();
+	}
+
+
+	// Method to implement which solves the network flow problem.
+	public abstract void solveBFS();
+	
+	public abstract void bigSolveBFS();
+
+
+}
