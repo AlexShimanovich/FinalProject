bfb039b350b4c0de8922d45fe329faf381ac8089
AlexShimanovich <37244900+AlexShimanovich@users.noreply.github.com>
2019-06-17 22:02:18 +0300

    some refactoring for result collection

--- Greed.java
+++ Greed.java
@@ -8,6 +8,35 @@ import java.util.LinkedList;
 
 class GreedGraph 
 { 
+
+	private static class Result{
+		private int SCC_Size;
+		private LinkedList<Integer> indexesOfSeparator;		
+
+		public Result(int SCC_Size, LinkedList<Integer> indexesOfSeparator) {
+			this.SCC_Size = SCC_Size;
+			this.indexesOfSeparator = new LinkedList<Integer>(indexesOfSeparator); 
+		}
+
+		public int getSccSize() {
+			return SCC_Size;
+		}
+
+		public LinkedList<Integer> getIndexesOfSeparator() {
+			return indexesOfSeparator;
+		}
+
+		public int getSeparatorSize() {
+			return indexesOfSeparator.size();	
+		}
+
+		public String toString() {
+			String res = "SCC size: " + SCC_Size + "\n Separator size: " + indexesOfSeparator.size();  
+			return res;
+		}
+
+	}
+
 	private int V; // No. of vertices in graph 
 	private LinkedList<Integer> adj[]; //Adjacency List Graph representation
 	private int rootArray[]; //track the origin of each node (same origin == same SCC)
@@ -17,11 +46,11 @@ class GreedGraph
 	//token to be used to know if node is part of separator, 
 	//instead of changing 0 to 1 and vice-versa in all the separatorArray[] I just do separatorToken++
 	private int separatorToken = 0; 
-	
+
 	//Constructor 
 	GreedGraph(int v){ 
 		V = v; 
-		adj = new LinkedList[v]; 
+		adj = new LinkedList[V]; 
 		separatorArray = new int[V];
 		indexesOfSeparator = new LinkedList();
 		for (int i = 0; i < v; ++i) 
@@ -50,6 +79,9 @@ class GreedGraph
 		} 
 		transposeGraph.rootArray = new int[V];
 		transposeGraph.sizeSccArray = new int[V];
+		transposeGraph.separatorArray = separatorArray;
+		transposeGraph.indexesOfSeparator = indexesOfSeparator;
+		transposeGraph.separatorToken = separatorToken;
 		return transposeGraph; 
 	} 
 
@@ -61,10 +93,10 @@ class GreedGraph
 
 		// Recursion for all the vertices adjacent to this vertex 
 		Iterator<Integer> i = adj[v].iterator(); 
-		while (separatorArray[v] != separatorToken && i.hasNext()) 
+		while (i.hasNext()) 
 		{ 
 			int n = i.next(); 
-			if(!visited[n] && separatorArray[n] != separatorToken) 
+			if(!visited[n] /*&& separatorArray[n] != separatorToken*/) 
 				fillDfsOrder(n, visited, stack); 
 		} 
 
@@ -72,33 +104,29 @@ class GreedGraph
 		// push v to Stack 
 		stack.push(new Integer(v)); 
 	} 
-	
+
 	// A recursive function to print DFS starting from v 
-	private void DFSUtil(int v, boolean visited[], int origin){ 
+	private void DFSUtil(int v, boolean visited[], int origin, boolean print){ 
 		// Mark the current node as visited and print it 
 		visited[v] = true;
-		if(separatorArray[v] != separatorToken)
-			return;
 		rootArray[v] = origin;
 		sizeSccArray[origin]++;
-		System.out.print(v + " "); 
-
-		int n; 
+		if(print)
+			System.out.print(v + " "); 
 
 		// Recursion for all the vertices adjacent to this vertex 
-		Iterator<Integer> i = adj[v].iterator(); 
-		while (i.hasNext()) 
-		{ 
-			n = i.next(); 
-			if (!visited[n] && separatorArray[n] != separatorToken) 
-				DFSUtil(n, visited, origin); 
-		} 
+        for(Integer node: adj[v]){ 
+        	//System.out.print("looking at node " + node);
+			if (!visited[node] && separatorArray[node] != separatorToken) 
+				DFSUtil(node, visited, origin, print); 
+        } 
+
 	} 
 
 	// The main function that finds and prints all strongly 
 	// connected components in Graph
 	// return the size of biggest SCC in the graph
-	private int printSCCs() 
+	private int findSCCs(boolean print) 
 	{ 
 		Stack stack = new Stack(); 
 
@@ -109,7 +137,7 @@ class GreedGraph
 
 		// Fill vertices in stack according to their finishing times 
 		for (int i = 0; i < V; i++) 
-			if (!visited[i] && separatorArray[i] != separatorToken) 
+			if (!visited[i] /*&& separatorArray[i] != separatorToken*/) 
 				fillDfsOrder(i, visited, stack); 
 
 		// Create a transposed graph 
@@ -118,9 +146,18 @@ class GreedGraph
 		// Mark all the vertices as not visited (For second DFS) 
 		for (int i = 0; i < V; i++) 
 			visited[i] = false; 
+/*
+        for(int v = 0; v < gr.V; v++) 
+        { 
+            System.out.println("Adjacency list of vertex "+ v); 
+            System.out.print("head"); 
+            for(Integer pCrawl: gr.adj[v]){ 
+                System.out.print(" -> "+pCrawl); 
+            } 
+            System.out.println("\n"); 
+        } */
 
-		
-		// Now process all vertices in order defined by Stack 
+		// Now process all vertices in transpose graph in order defined by Stack 
 		while (!stack.empty()){ 
 			// Pop a vertex from stack 
 			int v = (int)stack.pop(); 
@@ -128,53 +165,62 @@ class GreedGraph
 			// Print Strongly connected component of the popped vertex 
 			if (!visited[v] && separatorArray[v] != separatorToken) 
 			{ 
-				gr.DFSUtil(v, visited, v); //running on transposed graph
-				System.out.println(); 
+				gr.DFSUtil(v, visited, v, print); //running on transposed graph
+				if(print)
+					System.out.println(); //new line for new SCC
 			} 
 		}
-		System.out.println("Node Origins array: " + Arrays.toString(gr.rootArray));
-		System.out.println("SCC Size array: " + Arrays.toString(gr.sizeSccArray));
+
 		Arrays.sort(gr.sizeSccArray);
 		int biggestScc = gr.sizeSccArray[gr.sizeSccArray.length - 1];
-		System.out.println("Biggest SCC size is " + biggestScc);
+//		if(print) {
+//			System.out.println("Node Origins array: " + Arrays.toString(gr.rootArray));
+//			System.out.println("SCC Size array: " + Arrays.toString(gr.sizeSccArray));
+//			System.out.println("Biggest SCC size is " + biggestScc);
+//		}
 		return biggestScc;
 	} 
-	
+
 
 	//get random integer in range [min, max] (inclusive)
 	public int randomIntRange(int min, int max){
-	    int x = (int)(Math.random() * ( (max - min) + 1) ) + min;
-	    return x;
+		int x = (int)(Math.random() * ( (max - min) + 1) ) + min;
+		return x;
 	}
-	
+
 	//randomly select n different vertices from graph to be separator, 
 	//n must be smaller than V
 	//we make sure that with the random separator the biggest SCC is smaller than 2/3 of V 
 	public void randomSeparator(int n){
 		int node;
 		separatorToken++;
-		Random rand = new Random(); 
-		{
+		indexesOfSeparator.clear();
+		Random rand = new Random(); /*
+		do{
 			for(int i = 0; i < n; i++) {
 				do {
-					
 					node = rand.nextInt(V); // random between [0,V) - every node in graph can be part of separator	
 				}while(separatorArray[node] == separatorToken);	// make sure we select different node each time	
 				separatorArray[node] = separatorToken; //collect all indexes of the separator
 				indexesOfSeparator.add(node); //collect all indexes of the separator
 			}
-		}while(printSCCs() > (int)(0.666 * V)); //if this separator creates too big SCC look for another separator
+		}while(findSCCs(false) > (int)(0.666 * V)); //if this separator creates too big SCC look for another separator*/
+		separatorArray[3] = separatorToken; //collect all indexes of the separator
+		indexesOfSeparator.add(3); //collect all indexes of the separator
+		separatorArray[4] = separatorToken; //collect all indexes of the separator
+		indexesOfSeparator.add(4); //collect all indexes of the separator
 	}
-	
+
 	//move one random node from the separator back to graph
 	public void moveNodeBackToGraph(){
-	    Random rand = new Random(); 
-	    int randomIndex = rand.nextInt(indexesOfSeparator.size());
-	    int indexToMoveBack = indexesOfSeparator.get(randomIndex); //randomly select one index	
-	    separatorArray[indexToMoveBack]--; //remove it from the separator (it no longer equals current separator token)
-	    indexesOfSeparator.remove(randomIndex); //remove it from the indexes list
+		Random rand = new Random(); 
+		int randomIndex = rand.nextInt(indexesOfSeparator.size());
+		int indexToMoveBack = indexesOfSeparator.get(randomIndex); //randomly select one index	
+		separatorArray[indexToMoveBack]--; //remove it from the separator (it no longer equals current separator token)
+		indexesOfSeparator.remove(randomIndex); //remove it from the indexes list
+		System.out.println("moving back node " + indexToMoveBack);
 	}
-	
+
 	// Test engine
 	public static void main(String args[]) 
 	{ 
@@ -189,36 +235,42 @@ class GreedGraph
 		g.addEdge(5, 6);
 		g.addEdge(6, 4);
 
-//		int i = 0;
-//		while(i < 10) {
-//			System.out.println("Random shit: " + g.randomIntRange(0, max-1)); 
-//			i++;
-//		}
-		
-//		g.randomSeparator(2);
-//		for(int i = 0; i < g.V; i++) {
-//			System.out.println("Random shit: " + g.separatorArray[i]); 
-//		}
-		
+
 		int iteration = 0;
-		int iterationResults[] = new int[10]; //collect the best results from each iteration
-		while(iteration < 10) {
-			int separatorSize = 2;
+		int biggestSCC = 0;
+		LinkedList<Result> results = new LinkedList(); //collect the best results from each run
+		Result res = null;
+		while(iteration < 2) {
+			int separatorSize = 3;
 			g.randomSeparator(separatorSize); //create random separator in graph
 			System.out.println("Start ITERATION: " + iteration); 
 			System.out.println("Initial random separator is : " + Arrays.toString(g.indexesOfSeparator.toArray()));
- 
 			//each iteration runs while we can move nodes and biggest SCC is not too big 
 			do{
 				System.out.println("Separator now: " + Arrays.toString(g.indexesOfSeparator.toArray())); 
-				g.moveNodeBackToGraph()	; 
-			}while(g.printSCCs() > (int)(0.666 * g.V) && g.indexesOfSeparator.size() > 0);
+				g.moveNodeBackToGraph(); 
+				biggestSCC = g.findSCCs(true);
+				if(biggestSCC > (int)(0.666 * g.V)) {
+					System.out.println("Biggest scc is too big, size: " + biggestSCC + " DEAD END");
+					break;
+				}
+				if(biggestSCC > 1) { //collect interesting results
+					res = new Result(biggestSCC, g.indexesOfSeparator);	
+					results.add(res);
+				}
+
+			}while(g.indexesOfSeparator.size() > 1);
 
 			System.out.println("End ITERATION: " + iteration);
 			iteration++;
 		}
-		
-		//System.out.println("Those are the SCC of our graph:"); 
-		//g.printSCCs(); 
+
+		System.out.println("Lets look at some interesting results");
+		if(results.size() > 0) {
+			for(Result resIterator : results)
+				System.out.println(resIterator);
+		}
+
+
 	} 
 } 
